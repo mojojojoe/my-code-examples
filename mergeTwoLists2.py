2278aaac0a73a82dc12ce01e1b3baf96388b2eec
Bruce R Graham <bruce.graham@gmail.com>
2023-07-23 21:28:59 +0200

    Evening exercise: to get the divide and conquer binary tree working on a two list merge. The code is working but the algorithm is not there yet.

---
+++ mergeTwoLists2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.11
+
+def divideAndConquer(L:list)->list:
+    end = len(L)
+    m = end // 2
+    if end > 2:
+        divideAndConquer(L[m:])
+        divideAndConquer(L[:m])
+    elif end == 2:
+        a = L.pop()
+        b = L.pop()
+        if b > a:
+            L.append(b)
+            L.append(a)
+        else:
+            L.append(a)
+            L.append(b)
+        return L
+    else:
+        return L
+
+def mergeTwoLists(one,two):
+    for i in two:
+        one.append(i)
+    return divideAndConquer(one)
+
+
+
+def recurrence(list1:list,list2:list):
+    def iterate(L):
+        if len(L) == 1:
+            return L
+        else:
+            iterate(L[-1:])
+
+def main():
+    A, B = [], []
+    A = [20,17,14,11,8,5,3,2,1]
+    B = [22,20,18,16,14,12,10,8,6,4,2]
+    print(mergeTwoLists(A,B))
+ 
+if __name__ == '__main__':
+    main()
